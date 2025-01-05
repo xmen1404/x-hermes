e12f60fe7c7e6ad46cd399177dbb615ea87981c1
combi2k2 <cumbee2k2@gmail.com>
2025-01-05 21:27:03 +0700

    feat: add yaml-cpp dependency and wrapper for config usage

--- core/config/config.h
+++ core/config/config.h
@@ -1,20 +1,43 @@
 #pragma once
 
 #include <string>
+#include "yaml-cpp/yaml.h"
 #include <unordered_map>
 
 namespace xhermes::config {
+    class Config {
+        public:
+            template <typename T>
+            T Get(const std::string &key) {
+                return node_[key].as<T>();
+            }
+            bool Has(const std::string &key) {
+                if (node_[key]) return true;
+                else            return false;
+            }
+            Config() {}
+            Config(const YAML::Node &node) : node_(node) {}
+            Config(const std::string &filepath) {
+                node_ = YAML::LoadFile(filepath);
+            }
+            friend struct YAML::convert<xhermes::config::Config>;
 
-class Config {
-public: 
-  template <typename T>
-  T Get(const std::string key) {
-    return data_[key];
-  }
-
-private:
-  std::unordered_map<std::string, std::string> data_;
-};
-
+        private:
+            YAML::Node node_;
+    };
 } // namespace xhermes::config
+
+namespace YAML {
+    template<>
+    struct convert<xhermes::config::Config> {
+        static Node encode(const xhermes::config::Config& rhs) {
+            return rhs.node_;
+        }
+
+        static bool decode(const Node& node, xhermes::config::Config& rhs) {
+            rhs.node_ = node;
+            return true;
+        }
+    };
+}
 
